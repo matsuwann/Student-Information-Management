eef46e1c14239e54b7c42a2b0ebca857269140e0
mtthw <mtthw@Matsu>
2024-07-07 23:05:19 +0800

    indentation

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,20 +1,18 @@
 
 import java.awt.EventQueue;
 
-
 public class MainClass {
 
-   
     public static void main(String[] args) {
-      EventQueue.invokeLater(new Runnable() {
-         @Override
-         public void run(){
-           Login log = new Login(); 
-           log.setVisible(true);
-           log.setResizable(false);
-                
-         }  
-      });
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Login log = new Login();
+                log.setVisible(true);
+                log.setResizable(false);
+
+            }
+        });
     }
-    
+
 }
