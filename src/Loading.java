e724ad8bcddf3ff64b84f6914f76b96c7bc43b44
mtthw <mtthw@Matsu>
2024-07-07 23:03:03 +0800

    Made some adjustment

--- src/Loading.java
+++ src/Loading.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
@@ -5,48 +6,49 @@ import java.awt.Image;
 import javax.swing.*;
 
 public class Loading extends JFrame {
+
     JLabel lblPUP, logoLabel;
-    
+
     JLabel lblLoad;
     JProgressBar lbLoad;
-    
-    Loading(){
+
+    Loading() {
         setTitle("Polytechnic University of the Philippines - Biñan");
-        setSize(1000,800);
+        setSize(1000, 800);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setBackground(new Color(255,218,102));
-        
+        getContentPane().setBackground(new Color(255, 218, 102));
+
         ImageIcon icon = new ImageIcon("pup.png");
         setIconImage(icon.getImage());
-        
-        ImageIcon logoImage = new ImageIcon("pup.png"); // Provide the correct path to your logo image file
-        Image scaledLogoImage = logoImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH); // Resize image to 120x90
+
+        ImageIcon logoImage = new ImageIcon("pup.png"); 
+        Image scaledLogoImage = logoImage.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH); 
         ImageIcon scaledLogoIcon = new ImageIcon(scaledLogoImage);
         logoLabel = new JLabel(scaledLogoIcon);
-        logoLabel.setBounds(25, 15, 100, 100); // Set bounds to match resized image
+        logoLabel.setBounds(25, 15, 100, 100); 
         add(logoLabel);
-        
+
         lblPUP = new JLabel("Polytechnic University of the Philippines - Biñan");
         lblPUP.setBounds(150, 15, 1000, 100);
         lblPUP.setFont(new Font("Arial", Font.BOLD, 30));
         add(lblPUP);
-        
+
         //Loading Bar
         lblLoad = new JLabel("LOADING PLEASE WAIT");
-        lblLoad.setBounds(300,300,500,40);
+        lblLoad.setBounds(300, 300, 500, 40);
         lblLoad.setFont((new Font("Arial", Font.BOLD, 30)));
         add(lblLoad);
-        
+
         lbLoad = new JProgressBar();
         lbLoad.setValue(0);
         lbLoad.setStringPainted(true);
-        lbLoad.setBounds(230,350,500,30);
-        lbLoad.setBackground(new Color(245,237,207));
+        lbLoad.setBounds(230, 350, 500, 30);
+        lbLoad.setBackground(new Color(245, 237, 207));
         lbLoad.setForeground((Color.WHITE));
-        lbLoad.setFont(new Font("Arial",Font.PLAIN,12));
+        lbLoad.setFont(new Font("Arial", Font.PLAIN, 12));
         add(lbLoad);
-        
+
         new SwingWorker<Void, Void>() {
             @Override
             public Void doInBackground() throws Exception {
@@ -57,26 +59,23 @@ public class Loading extends JFrame {
             @Override
             public void done() {
                 dispose();
-                Viewing view = new Viewing();           
+                Viewing view = new Viewing();
                 view.setVisible(true);
             }
         }.execute();
     }
-    private void fill()
-    {
+
+    private void fill() {
         int i = 0;
-        try
-        {
-            while (i<100)
-            {
+        try {
+            while (i < 100) {
                 lbLoad.setValue(i + 6);
                 Thread.sleep(120);
                 i += 10;
             }
+        } 
+        catch (Exception e) {
         }
-        catch(Exception e)
-        {
-        }
-        
+
     }
 }
